7a4913e60977631e697dc7b6043842fad4d5e4fc
Luzyanin Kirill <jk87@rambler.ru>
2023-03-20 01:25:45 +0300

    20-03-2023

--- 17-03-2023 (JavaScript)/Homework 17-03-2023/script.js
+++ 17-03-2023 (JavaScript)/Homework 17-03-2023/script.js
@@ -1,30 +1,53 @@
-const newBlockNews = document.createElement("div");
-//newBlockNews.setAttribute("class", "news");
-newBlockNews.classList.add("news");
-
-const newBlockNewsTitle = document.createElement("div");
-newBlockNewsTitle.classList.add("news_title");
-
-const newBlockNewsSubtitle = document.createElement("h3");
-newBlockNewsSubtitle.classList.add("news_subtitle");
-
-const newBlockNewsTime = document.createElement("div");
-newBlockNewsTime.classList.add("news_time");
-
-
 const textNewsTitle = document.createTextNode("спорт");
 const textNewsSubtitle = document.createTextNode("Макдэвид побил рекорд по очкам для игроков в XXI веке");
 const textNewsTime = document.createTextNode("03:00");
 
 
+const newBlockNewsTitle = document.createElement("div");
+newBlockNewsTitle.classList.add("news_title");
 newBlockNewsTitle.appendChild(textNewsTitle);
+
+const newBlockNewsSubtitle = document.createElement("h3");
+newBlockNewsSubtitle.classList.add("news_subtitle");
 newBlockNewsSubtitle.appendChild(textNewsSubtitle);
+
+const newBlockNewsTime = document.createElement("div");
+newBlockNewsTime.classList.add("news_time");
 newBlockNewsTime.appendChild(textNewsTime);
 
 
-newBlockNews.appendChild(newBlockNewsTitle, newBlockNewsSubtitle, newBlockNewsTime);
+const newBlockNews = document.createElement("div");
+//newBlockNews.setAttribute("class", "news");
+newBlockNews.classList.add("news");
+//newBlockNews.appendChild(newBlockNewsTitle, newBlockNewsSubtitle, newBlockNewsTime);
+newBlockNews.appendChild(newBlockNewsTitle);
 
 
-//element.setAttribute("class", "selected-text");
+// вариант с пустым div в index.html
+document.getElementById("news_empty").appendChild(newBlockNewsTitle, newBlockNewsSubtitle, newBlockNewsTime);
+
+document.getElementById("news_empty").appendChild(newBlockNewsTitle);
+
+
+
+let divNews = document.getElementsByClassName("news");
+//console.log(divNews); //  выводит все три
+let divNewsId = document.getElementById("news");
+console.log(divNewsId); // выводит первый попавшийся?
+//divNews
 
 //document.body.getElementsByClassName("news").appendChild(newBlockNews);
+
+
+
+
+
+// -------------------------смена фона-------------------------------
+let sportNews = document.querySelectorAll(".news_title");
+sportNews.forEach((element)=>{
+    if(element.innerText == "спорт") {
+        sportNews.backgroundColor = "red";
+        //element.style.backgroundColor = "red"; // так меняется только у элемента фон..
+    }
+}
+);
