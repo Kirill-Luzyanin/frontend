922e41f409034c3cb0e9489590e8144603ccc7bc
Luzyanin Kirill <jk87@rambler.ru>
2023-03-27 00:04:55 +0300

    ДЗ от 24-03-2023+

--- 24-03-2023 (JavaScript4)/Homework 24-03-2023 (Input+Array)/script.js
+++ 24-03-2023 (JavaScript4)/Homework 24-03-2023 (Input+Array)/script.js
@@ -1,34 +1,29 @@
+const users = [];
+
 function newUser(p1, p2) {
-    return {
+    let user = {
         firstName: p1,
         lastName: p2
     }
-}
-
-const users = [];
-
-function addToArray(p1, p2) {
-    return users.push(newUser(p1, p2));
+    users.push(user);
 }
 
 document.getElementById("button_next").addEventListener("click",
     () => {
         // считывание значений из input:
-        let name = document.getElementById("name_input-id").value;
-        let surname = document.getElementById("surname_input-id").value;
-        //let name = document.getElementByTagName("input")[0].value; // почему так не работает???
-        //let surname = document.getElementByTagName("input")[1].value; // почему так не работает???
+        //let name = document.getElementById("name_input-id").value;
+        //let surname = document.getElementById("surname_input-id").value;
+        let name = document.getElementsByTagName("input")[0].value; 
+        let surname = document.getElementsByTagName("input")[1].value; 
+        console.log(name);
+        console.log(surname);
 
         // выполнение метода по созданию нового пользователя:
         newUser(name, surname);
-        console.log(newUser(name, surname));
+        console.log(users);
 
-        // выполнение метода по добавлению нового пользователя в массив:
-        addToArray(name, surname);
-        console.log(addToArray(name, surname));
-
-        //document.querySelector('input').value = ''; ///Если так, то только одно поле очищается. All не работает.
-        // как правильно обнулять input-placeholder?
+        // очистка input
+        //document.querySelectorAll('input').value = ''; ///Если так, то получаем массив.
         document.getElementById("name_input-id").value = '';
         document.getElementById("surname_input-id").value = '';
 
