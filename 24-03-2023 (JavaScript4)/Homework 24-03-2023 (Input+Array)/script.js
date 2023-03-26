6c59fd0d9991e212b84ed7837293d717289285b0
Luzyanin Kirill <jk87@rambler.ru>
2023-03-26 19:15:01 +0300

    ДЗ от 24-03-2023+\

--- 24-03-2023 (JavaScript4)/Homework 24-03-2023 (Input+Array)/script.js
+++ 24-03-2023 (JavaScript4)/Homework 24-03-2023 (Input+Array)/script.js
@@ -1,9 +1,3 @@
-let name = document.getElementById("name_input-id").value;
-let surname = document.getElementById("surname_input-id").value;
-
-//let name1 = document.getElementByTagName("input")[0].value;
-//let surname1 = document.getElementByTagName("input")[1].value;
-
 function makeUser(name,surname) {
     return {
         firstName: name,
@@ -11,12 +5,36 @@ function makeUser(name,surname) {
     }
 }
 
-//let user1 = makeUser("John", 30);
-//console.log(user1);
+function addToArray(){
+    const users = [];
+    //users.length=20;
+    for (let index = 0; index < 10; index++) {
+        users.push(users[index]);
+        console.log(users[index]);
+    }
+ }
 
 document.getElementById("button_next").addEventListener("click",
     () => {
+        let name = document.getElementById("name_input-id").value;
+        let surname = document.getElementById("surname_input-id").value;
+        //let name = document.getElementByTagName("input")[0].value; // почему так не работает???
+        //let surname = document.getElementByTagName("input")[1].value;
+        
         makeUser(name,surname);
+        console.log(makeUser(name,surname)); 
+        
+        addToArray(name, surname);
+        console.log(addToArray());
+
+        //document.querySelector('input').value = ''; ///Если так, то только одно поле очищается. All не работает.
+        // как правильно обнулять input-placeholder
+        document.getElementById("name_input-id").value = '';
+        document.getElementById("surname_input-id").value = '';
+
+        
     }
 );
+
+
 
