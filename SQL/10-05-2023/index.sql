e56fc5dbeaa2a7c7cdb997096344fe15ba72e972
Luzyanin Kirill <jk87@rambler.ru>
2023-05-17 10:25:14 +0300

    10-05-2023 ДЗ

--- SQL/10-05-2023/index.sql
+++ SQL/10-05-2023/index.sql
@@ -1,26 +1,26 @@
-<!--1 . Вывести телефоны всех поставщиков, кроме тех, что из Germany и France. -->
+/* 1 . Вывести телефоны всех поставщиков, кроме тех, что из Germany и France. */
 SELECT Phone 
 FROM Suppliers
 WHERE Country NOT IN ("Germany", "France")
 
-<!-- 2. Для товаров до 10 EUR установить категорию 8 и применить наценку в 20%. -->
+-- 2. Для товаров до 10 EUR установить категорию 8 и применить наценку в 20%.
 UPDATE Products
 SET 
 	CategoryID=8,
     Price=Price * (1+.2)
 WHERE Price <= 10
 
-<!-- 3. Удалить поставщиков из USA и Germany. -->
+-- 3. Удалить поставщиков из USA и Germany.
 DELETE FROM Suppliers
 WHERE Country IN ("USA", "Germany")
 
-<!-- 4. Добавить два поставщика из Spain с произвольными номерами телефонов. -->
+-- 4. Добавить два поставщика из Spain с произвольными номерами телефонов.
 INSERT INTO Suppliers (SupplierName, ContactName, Address, City, PostalCode, Country, Phone)
 VALUES 
 	(NULL, NULL, NULL, NULL, NULL, 'Spain', '1111'),
     (NULL, NULL, NULL, NULL, NULL, 'Spain', '2222');
 
-<!-- 5. Вывести все товары до 20 EUR из категорий 3 и 6 по убыванию цены. -->
+-- 5. Вывести все товары до 20 EUR из категорий 3 и 6 по убыванию цены.
 SELECT * 
 FROM Products
 WHERE 
